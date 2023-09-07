eac220a45fe6fdfa66672c5a8ed591ee182f808a
Robert Crumbaugh <chasecrumbaugh4@gmail.com>
2023-09-07 16:34:02 -0700

    wip

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,35 @@
 import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import {DocsThemeConfig} from 'nextra-theme-docs'
+import {useRouter} from "next/router";
+import {Header, slugString} from "./lib";
+
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+    logo: <span>My Project</span>,
+    project: {
+        link: 'https://github.com/shuding/nextra-docs-template',
+    },
+    chat: {
+        link: 'https://discord.com',
+    },
+    docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+    footer: {
+        text: 'Nextra Docs Template',
+    },
+    sidebar: {
+        titleComponent({title}) {
+            const router = useRouter()
+            const pageTitle = title.split("/").pop()
+
+            const slug = slugString(router);
+            const titleSlug = title.toLowerCase()
+            const selected = slug === titleSlug
+            return <div style={{background: selected ? "cyan" : "none"}}>{pageTitle}</div>
+        }
+    },
+    components: {
+        h2: ({children}) => <Header>{children}</Header>
+    },
 }
 
 export default config
