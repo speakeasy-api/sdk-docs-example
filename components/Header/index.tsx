d9596fbb9d2e2909c433ba55d0a100430a99d0e1
Dima Kozoliy <dima05191@gmail.com>
2023-09-13 16:09:34 +0200

    transfered main styles and components header, footer from next app; updated config of nextra theme; added sass support; updated tsconfig

---
+++ components/Header/index.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from 'react';
+import NextLink from 'next/link';
+
+import Logo from '@/icons/Logo';
+import ThemeToggle from '@/components/ThemeToggle';
+
+import styles from './styles.module.scss';
+
+const Header: FC = () => {
+  return (
+    <div className={styles.headerRoot}>
+      <div className={styles.headerInner}>
+        <div>
+          <NextLink href="/">
+            <Logo />
+          </NextLink>
+        </div>
+        <div>
+          <ThemeToggle />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import App from 'next/app'
-import {ScrollManager} from "../components/scrollHelpers";
+import React from 'react';
+import App from 'next/app';
+import { Inter } from 'next/font/google';
+
+import { ScrollManager } from '../components/scrollHelpers';
+
+const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+});
 
 class MyApp extends App {
-    render() {
-        const {Component, pageProps} = this.props
-        return <ScrollManager>
-            <Component {...pageProps} />
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        <style jsx global>{`
+          html {
+            font-family: ${inter.style.fontFamily};
+          }
+        `}</style>
+        <ScrollManager>
+          <Component {...pageProps} />
         </ScrollManager>
-    }
+      </>
+    );
+  }
 }
 
-export default MyApp
+export default MyApp;
