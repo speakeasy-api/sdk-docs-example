f103adaf109fdf0cd7af88644cbfcf50bfc2ba31
Dima Kozoliy <dima05191@gmail.com>
2023-09-15 00:50:12 +0200

    code refactoring; added all necessary configs in root of project

--- content/_app.tsx
+++ content/_app.tsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import App from 'next/app'
-import {ScrollManager} from "../components/scrollManager";
-import {LanguageProvider} from "../components/languageHelpers";
+import React from 'react';
+import App from 'next/app';
+
+import { ScrollManager } from '../components/scrollManager';
+import { LanguageProvider } from '../components/languageHelpers';
 
 class MyApp extends App {
-    render() {
-        const {Component, pageProps} = this.props
-        return <ScrollManager>
-            <LanguageProvider>
-                <Component {...pageProps} />
-            </LanguageProvider>
-        </ScrollManager>
-    }
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return <ScrollManager>
+      <LanguageProvider>
+        <Component {...pageProps} />
+      </LanguageProvider>
+    </ScrollManager>;
+  }
 }
 
-export default MyApp
+export default MyApp;
--- theme.config.tsx
+++ theme.config.tsx
@@ -1,44 +1,47 @@
-import React from 'react'
-import {DocsThemeConfig} from 'nextra-theme-docs'
+import { DocsThemeConfig } from 'nextra-theme-docs';
 
 import ThemeToggle from '@/components/ThemeToggle';
-import {Header} from './components/header';
-import {OramaSearch} from '@orama/plugin-nextra'
-import {NavItem} from "./components/navItem";
-import Collapsible from "./components/collapsible";
+import Logo from '@/icons/Logo';
+import Footer from '@/components/Footer';
+
+import { Header } from './components/header';
+import { OramaSearch } from '@orama/plugin-nextra';
+import { NavItem } from './components/navItem';
+import Collapsible from './components/collapsible';
 
 const config: DocsThemeConfig = {
-    logo: <span>My Project</span>,
-    project: {
-        link: 'https://github.com/shuding/nextra-docs-template',
-    },
-    chat: {
-        link: 'https://discord.com',
-    },
-    docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-    footer: {
-        text: 'Nextra Docs Template',
-    },
-    sidebar: {
-        titleComponent: NavItem,
-    },
-    toc: {
-        component: () => null,
-    },
-    components: {
-        h1: Header("h1"),
-        h2: Header("h2"),
-        Collapsible,
-    },
-    search: {
-        component: () => OramaSearch(),
-    },
-    navbar: {
-        extraContent: ThemeToggle,
-    },
-    navigation: false,
-    gitTimestamp: () => null,
-    darkMode: false,
-}
+  logo: Logo,
+  project: {
+    link: 'https://github.com/shuding/nextra-docs-template',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    component: Footer,
+  },
+  sidebar: {
+    titleComponent: NavItem,
+  },
+  toc: {
+    component: () => null,
+  },
+  components: {
+    h1: Header('h1'),
+    h2: Header('h2'),
+    Collapsible,
+    blockquote: Collapsible,
+  },
+  search: {
+    component: () => OramaSearch(),
+  },
+  navbar: {
+    extraContent: ThemeToggle,
+  },
+  navigation: false,
+  gitTimestamp: () => null,
+  darkMode: false,
+};
 
-export default config
+export default config;
