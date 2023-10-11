4d6d5d4042fdc1144cff1ffc735775e7a9439bcd
Ryan Albert <ryantimalbert@gmail.com>
2023-10-11 15:47:59 -0700

    update

--- content/_app.tsx
+++ content/_app.tsx
@@ -1,12 +1,40 @@
-import React from 'react';
-import App from 'next/app';
+import React, { ReactElement } from 'react';
+import { Columns, RHS } from '@/components/columns';
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
+import TextHeaderWrapper from '@/HOC/TextHeaderWrapper';
+import LanguageSwitcher from '@/HOC/LanguageSwitcher';
+import newLanguageProvider from '@/HOC/LanguageProvicer';
 
-    return <Component {...pageProps} />;
-  }
-}
+// REPLACE USAGE WITH HOC/LanguageProvider
+export const LanguageProvider = newLanguageProvider;
 
-export default MyApp;
+// REPLACE USAGE WITH HOC/LanguageProvider
+/* export const LanguageSelect = () => {
+    const langContext = useContext(LangContext);
+    return (
+        <button style={{background: "tomato", margin: "24px", padding: "10px"}}
+                onClick={() => langContext.setLang(langContext.lang === "go" ? "typescript" : "go")}>
+            Language:
+            {langContext.lang}
+        </button>
+    )
+} */
+
+// REPLACE USAGE WITH HOC/LanguageSwitcher
+export const LanguageSwitch = LanguageSwitcher;
+
+export const LanguageOperation = (props: {
+  usage: ReactElement;
+  parameters: ReactElement;
+  response: ReactElement;
+}) => {
+  return (
+    <Columns>
+      <TextHeaderWrapper headingType='h3'>Parameters</TextHeaderWrapper>
+      {props.parameters}
+      <TextHeaderWrapper headingType='h3'>Response</TextHeaderWrapper>
+      {props.response}
+      <RHS>{props.usage}</RHS>
+    </Columns>
+  );
+};
