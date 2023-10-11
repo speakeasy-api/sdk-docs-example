7c9a4c03d9e27589e14495e6da8bf23ec061da7f
Ryan Albert <ryantimalbert@gmail.com>
2023-10-11 15:12:46 -0700

    feat: dynamic collapsible component

--- components/collapsible.tsx
+++ components/collapsible.tsx
@@ -1,27 +1,54 @@
-import React, {ReactNode} from "react";
-import {splitAround, typeMatches} from "./typeHelpers";
+import React, { ReactNode, useState, useEffect } from 'react';
+import { splitAround, typeMatches } from './typeHelpers';
 
 type propsType = {
-    children: ReactNode[]
-    defaultOpen?: boolean
+    children: ReactNode[];
+    defaultOpen?: boolean;
+    content?: () => Promise<any>;
 }
 
 const Collapsible: React.FC<propsType> & { Break: typeof Break } = (props: propsType) => {
-    const elements = props.children
-    const [isOpen, setIsOpen] = React.useState(!!props.defaultOpen);
+    const { content, children } = props;
+    const [isOpen, setIsOpen] = useState(!!props.defaultOpen);
+    const [ContentComponent, setContentComponent] = useState<any>(null);
 
-    const [summarySection, bodySection] = splitAround(elements, (e) => typeMatches(e, Break));
+    useEffect(() => {
+        if (isOpen && content && !ContentComponent) {
+            content()
+                .then((module) => {
+                    setContentComponent(() => module.default);
+                })
+                .catch((error) => {
+                    console.error("Failed to load the content component", error);
+                });
+        }
+    }, [isOpen, content, ContentComponent]);
 
-    return <div style={{border: "1px solid green"}}>
-        <div onClick={() => setIsOpen((prev) => !prev)}>{summarySection}{isOpen ? "-" : "+"}</div>
-        {isOpen && <div style={{padding: "8px"}}>{bodySection}</div>}
-    </div>
-}
+    const dynamicChildren = isOpen && ContentComponent
+        ? [<ContentComponent key="dynamicContentComponent" />]
+        : [];
 
-const Break = () => {
-    return <></>
-}
+    const allChildren = children
+        ? [...children, ...dynamicChildren]
+        : [...dynamicChildren];
 
-Collapsible.Break = Break
+    const [summarySection, bodySection] = allChildren && allChildren.length
+        ? splitAround(allChildren, (e) => typeMatches(e, Break))
+        : [[], []];
 
-export default Collapsible
+    return (
+        <div style={{ border: '1px solid green' }}>
+            <div onClick={() => setIsOpen((prev) => !prev)}>
+                {summarySection}
+                {isOpen ? '-' : '+'}
+            </div>
+            {isOpen && <div style={{ padding: '8px' }}>{bodySection}</div>}
+        </div>
+    );
+};
+
+const Break = () => <></>;
+
+Collapsible.Break = Break;
+
+export default Collapsible;
