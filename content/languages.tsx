7a414b5e0b9dcdbbbf05ff12af409fad7b5c1a85
Ryan Albert <ryantimalbert@gmail.com>
2023-10-18 16:03:57 -0700

    test_diff

--- content/languages.tsx
+++ content/languages.tsx
@@ -1,46 +1,15 @@
-import React, {
-  Children,
-  ReactElement,
-  ReactNode,
-  useContext,
-  useState,
-} from 'react';
+import React, { ReactElement } from 'react';
+
+import LanguageSwitcher from '@/HOC/LanguageSwitcher';
+import newLanguageProvider from 'HOC/LanguageProvider';
 import { Columns, RHS } from '@/components/Columns';
 import { Parameters, Response } from '@/components/parameters';
-import { LanguageContext } from '@/utils/contexts/languageContext';
-import { LinkableContext } from '@/utils/contexts/linkableContext';
 
-export const Languages = ['go', 'typescript'];
-export type Language = (typeof Languages)[number];
+// REPLACE USAGE WITH HOC/LanguageProvider
+export const LanguageProvider = newLanguageProvider;
 
-export const LanguageProvider = (props: { children: ReactNode }) => {
-  const [language, setLanguage] = useState<Language>('go');
-  const context = {
-    language,
-    setLanguage,
-    languages: Languages,
-  };
-
-  const childrenArray = Children.toArray(props.children);
-
-  return (
-    <LanguageContext.Provider value={context}>
-      {childrenArray.map((child) => child)}
-    </LanguageContext.Provider>
-  );
-};
-
-export const LanguageSwitch = (props: {
-  langToContent: Partial<Record<Language, JSX.Element>>;
-}) => {
-  const { language } = useContext(LanguageContext);
-
-  return (
-    <LinkableContext.Provider value={false}>
-      {props.langToContent[language]}
-    </LinkableContext.Provider>
-  );
-};
+// REPLACE USAGE WITH HOC/LanguageSwitcher
+export const LanguageSwitch = LanguageSwitcher;
 
 export const LanguageOperation = (props: {
   usage: ReactElement;
